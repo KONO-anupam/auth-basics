e22fb0e9538b1d8295b2bfab7159682864306a4d
Kono <anupam.nitrkl@gmail.com>
2025-07-03 03:27:13 +0530

    feat: added password comparision

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /////// app.js
-
+const bcrypt = require('bcryptjs')
 const path = require("node:path");
 const { Pool } = require("pg");
 const express = require("express");
@@ -53,9 +53,11 @@ passport.use(
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       }
-      if (user.password !== password) {
-        return done(null, false, { message: "Incorrect password" });
-      }
+     const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+  // passwords do not match!
+    return done(null, false, { message: "Incorrect password" })
+    } 
       return done(null, user);
     } catch(err) {
       return done(err);
