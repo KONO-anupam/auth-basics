5b42baa2c6b7a13e7c0674afd630b327fac3ccd4
Kono <anupam.nitrkl@gmail.com>
2025-07-03 02:26:42 +0530

    feat: first step of auth

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require('passport-local').Strategy;
+require('dotenv').config()
 
 const pool = new Pool({
-  // add your configuration
+    connectionString:process.env.DATABASE_URL
 });
+
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
@@ -20,6 +22,34 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.render("index"));
-
 app.get('/sign-up',(req,res)=>res.render('sign-up-form'))
+app.post('/sign-up', async (req,res,next)=>{
+    try{
+        await pool.query(`INSERT INTO users (username,password) VALUES ($1,$2)`,[req.body.username,req.body.password,]);
+        res.redirect('/')
+    }
+    catch(error){
+        return next(error)
+    }
+})
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch(err) {
+      return done(err);
+    }
+  })
+);
+
 app.listen(3000, () => console.log("app listening on port 3000!"));
