9cb620a077cbe5ba8ff88eaab1c4402727896e16
Kono <anupam.nitrkl@gmail.com>
2025-07-03 01:52:46 +0530

    feat:added a sign up form

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-const express = require('express');
+/////// app.js
+
+const path = require("node:path");
+const { Pool } = require("pg");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+
+const pool = new Pool({
+  // add your configuration
+});
+
 const app = express();
-require('dotenv').config();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-const PORT =process.env.PORT || 3000;
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
-app.listen(PORT, ()=>{
-    console.log(`server is running on port ${PORT}`);
-})
+app.get("/", (req, res) => res.render("index"));
+
+app.get('/sign-up',(req,res)=>res.render('sign-up-form'))
+app.listen(3000, () => console.log("app listening on port 3000!"));
