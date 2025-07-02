e858d5ed6ade824abfc782c649a6e00cc1e81b13
Kono <anupam.nitrkl@gmail.com>
2025-07-03 03:08:31 +0530

    feat: added crypting hashin

--- app.js
+++ app.js
@@ -22,7 +22,16 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.render("index",{user: req.user}));
-app.get('/sign-up',(req,res)=>res.render('sign-up-form'))
+app.post("/sign-up", async (req, res, next) => {
+ try {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  await pool.query("insert into users (username, password) values ($1, $2)", [req.body.username, hashedPassword]);
+  res.redirect("/");
+ } catch (error) {
+    console.error(error);
+    next(error);
+   }
+});
 app.get('/log-out',(req,res,next)=>{
     req.logout((err) => {
     if (err) {
